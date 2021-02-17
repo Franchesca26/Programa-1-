1333e5a441657087cef0687f5c1c3728405076c1
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-02-17 13:04:54 -0400

    Estructura Repetitiva 2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,16 @@
 ﻿using System;
 
-namespace Estructura_Rep_1
+namespace Estructura_Rep_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numero1;
-            int numero2;
-            int resultado;
+            char letra;
 
-            do
-            {
+            for (letra = 'Z'; letra >= 'A'; letra--)
+                Console.Write("{0} ", letra);
 
-                Console.Write("Escriba un numero positivo:");
-                numero1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Escriba otro numero positivo:");
-                numero2 = Convert.ToInt32(Console.ReadLine());
-                resultado = numero1 + numero2;
-                if (numero1 != 0 || numero2 != 0)
-
-                    Console.WriteLine("El resultado es{0}", resultado );
-
-            } while (numero1 != 0 || numero2 != 0);
-            Console.WriteLine("Haga click para salir del programa...");
         }
     }
 }
