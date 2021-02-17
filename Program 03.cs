11b4f56678aaa256c1f6a7cf1d3b352fcd6bbdd5
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-02-17 13:14:21 -0400

    Estructura Repetitiva 3

---
+++ Program 03.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Estructura_Rep_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=10; i<=100;i=i+10) {
+
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
