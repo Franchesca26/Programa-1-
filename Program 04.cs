0ed7aed6276471aeacf313f0eb95e2549f33ee44
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-02-17 13:15:24 -0400

    Add files via upload

---
+++ Program 04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Estructura_Rep_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+
+            do
+            {
+                if (x <= 10)
+                {
+                    Console.Write(x);
+                    Console.Write(" - ");
+                    x = x + 1;
+                }
+            }
+            while (x != 1);
+            Console.ReadKey(); 
+             
+
+        }    
+    }
+
+}
